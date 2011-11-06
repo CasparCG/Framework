929f33337ce4fe02160328f9f0b3f853bfb12015
nirklas <nirklas@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2011-11-06 09:00:46 +0000

    Feature complete. needs fix for decoding of multibyte characters and refactoring of serverconnection and serverlistener by collection common functionality i common base-class

--- framework/trunk/Svt.Network/RemoteHostState.cs
+++ framework/trunk/Svt.Network/RemoteHostState.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Net.Sockets;
 using System.Net;
+using System.Threading;
 
 namespace Svt.Network
 {
     public class RemoteHostState
     {
+        internal ReaderWriterLockSlim streamLock = new ReaderWriterLockSlim();
+
         internal RemoteHostState(TcpClient client)
         {
+            SendQueue = new Queue<byte[]>();
+
             Client = client;
             Stream = client.GetStream();
             ReadBuffer = new byte[1024];
@@ -19,32 +24,105 @@ namespace Svt.Network
 
         internal void Send(byte[] data)
         {
-            //TODO: Make async, keep track of send-queue
-            if (data != null && data.Length > 0)
-                Stream.Write(data, 0, data.Length);
+            if (!_closed)
+            {
+                bool doNotify = false;
+                lock (SendQueue)
+                {
+                    SendQueue.Enqueue(data);
+                    if (SendQueue.Count == 1)
+                        doNotify = true;
+                }
+
+                if (doNotify)
+                    OnGotDataToSend();
+            }
         }
 
         internal bool Close()
         {
-            if (Stream != null)
+            bool result = true;
+
             {
-                Stream.Close();
-                Stream = null;
+                streamLock.EnterReadLock();
+                try
+                {
+                    if (Stream != null)
+                        Stream.Close();
+
+                    result = (Client != null);
+                    if (Client != null)
+                        Client.Close();
+                }
+                finally
+                {
+                    streamLock.ExitReadLock();
+                }
+            }
+            _closed = true;
+            {
+                //This is the only write-lock on streamLock
+                streamLock.EnterWriteLock();
+                try
+                {
+                    Stream = null;
+                    Client = null;
+                }
+                finally
+                {
+                    streamLock.ExitWriteLock();
+                }
             }
 
-            bool result = (Client != null);
-            if (Client != null)
-            {
-                Client.Close();
-                Client = null;
-            }
+            lock (SendQueue)
+                SendQueue.Clear();
+
             return result;
         }
 
-		public bool Connected { get { return (Client != null) ? Client.Connected : false; } }
-        public NetworkStream Stream { get; private set; }
-        public TcpClient Client { get; private set; }
-        public byte[] ReadBuffer { get; private set; }
+        volatile bool _closed = false;
+
+        //Protected by a ReaderWriterLock manually. Every access to the stream from ServerConnection/ServerListener need to be protected by a read-lock
+        internal NetworkStream Stream { get; private set; }
+        TcpClient Client { get; set; }
+
+        //does not need protection. usage is synchronous
+        internal byte[] ReadBuffer { get; private set; }
+
+        //Is accessed by multiple threads, needs to be protected by a critical section
+        internal Queue<byte[]> SendQueue { get; private set; }
+
+        //this read of Client should be protected as well.
+        public bool Connected 
+        { 
+            get 
+            {
+                streamLock.EnterReadLock();
+                try
+                {
+                    return (Client != null) ? Client.Connected : false;
+                }
+                finally { streamLock.ExitReadLock(); }
+            } 
+        }
+
         public IPEndPoint EndPoint { get; private set; }
+        public object ProtocolState { get; set; }
+
+        internal event EventHandler<EventArgs> GotDataToSend;
+        protected void OnGotDataToSend()
+        {
+            try
+            {
+                if (!_closed && GotDataToSend != null)
+                    GotDataToSend(this, EventArgs.Empty);
+            }
+            catch { }
+        }
+
+        public override string ToString()
+        {
+            return EndPoint.Address.ToString();
+        }
     }
 }
